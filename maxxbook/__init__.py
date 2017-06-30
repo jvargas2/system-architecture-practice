bd08064d65310a96f754cc025a6bd9e0e3bc9b91
Jorge Vargas <jorge@maxxpotential.com>
2017-06-30 07:51:52 -0400

    Update configs and install flask migrate

--- maxxbook/__init__.py
+++ maxxbook/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from maxxbook.config import BaseConfig
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
--- maxxbook/config.py
+++ maxxbook/config.py
@@ -3,5 +3,5 @@ import os
 class BaseConfig(object):
     DEBUG = True
     SECRET_KEY = 'development key'
-    #SQLALCHEMY_DATABASE_URI = 'postgres://postgres@postgres:5432/maxxbook_dev'
-    SQLALCHEMY_DATABASE_URI = os.environ['FLASK_DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['FLASK_DATABASE_URI'] # postgres://postgres@postgres:5432/maxxbook_dev
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
